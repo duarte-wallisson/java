abac906c27b0ab4d8a044f8983a1efaf592de4f0
duarte-wallisson <wallisson.elduarte@gmail.com>
2023-05-25 15:10:14 -0300

    refactor: change Map to classes and Comparator

--- exercise01/src/ManipulatingFiles.java
+++ exercise01/src/ManipulatingFiles.java
@@ -4,45 +4,42 @@ import java.io.IOException;
 import java.util.*;
 
 public class ManipulatingFiles {
-    public static Map<String, Double> toRead(String path){
-        Map<String, Double> itemsMap = new HashMap<>();
-        try (BufferedReader br =  new BufferedReader(new FileReader(path))){
+    public static List<Product> toRead(String path) {
+        List<Product> products = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             var line = br.readLine();
             while (line != null) {
                 String[] values = line.split(",");
                 String item = values[0].trim();
                 double price = Double.parseDouble(values[1].trim());
+                Product product = new Product(item, price);
 
                 System.out.println("Item: " + item);
                 System.out.println("Price: " + price);
-                itemsMap.put(item, price);
+                products.add(product);
                 line = br.readLine();
             }
             System.out.println("--------------");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return itemsMap;
+        return products;
     }
 
-    public static Double avgPrice(Map<String, Double> items) {
+    public static Double avgPrice(List<Product> items) {
         Double avg = 0D;
-        for (Double price: items.values()) {
-            avg += price;
+        for (Product item : items) {
+            avg += item.getPrice();
         }
         avg /= items.size();
-
         return avg;
     }
 
-    public static void  sort(Map<String, Double> items) {
-        List<Map.Entry<String, Double>> itemsList = new ArrayList<>(items.entrySet());
-        itemsList.sort(Map.Entry.<String, Double>comparingByValue().reversed());
-        for (Map.Entry<String, Double> entry : itemsList) {
-            String item = entry.getKey();
-            Double price = entry.getValue();
-            System.out.println("Item: " + item);
-            System.out.println("Price: " + price);
+    public static void sort(List<Product> items) {
+        items.sort(new MyComparator().reversed());
+        for (Product product : items) {
+            System.out.println("Item: " + product.getName());
+            System.out.println("Price: " + product.getPrice());
         }
     }
 }
