267ad1b281566927f97f79a59b23e1f150d161d6
duarte-wallisson <wallisson.elduarte@gmail.com>
2023-05-25 12:04:15 -0300

    feat: reading v2: with try-with-resources.

--- reading/src/Main.java
+++ reading/src/Main.java
@@ -5,18 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         File file = new File("C:\\Developer\\Projects\\java\\files\\text.txt");
-        Scanner sc = null;
-        try {
-            sc = new Scanner(file);
+        try (Scanner sc = new Scanner(file)) {
             while (sc.hasNextLine()) {
                 System.out.printf(sc.nextLine());
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
-        } finally {
-            if (sc != null) {
-                sc.close();
-            }
         }
     }
 }
